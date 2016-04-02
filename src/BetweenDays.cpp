8114d4aa2a8d304de3cc4c1463d1351d551bef3b
sheshank3173 <sheshank3173@missionrnd.com>
2016-04-02 21:02:33 +0530

    Submit Commit

--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -30,5 +30,111 @@ struct node{
 
 
 int between_days(struct node *date1head, struct node *date2head){
-	return -1;
+	int count1, count2, leap, year1, month1, day1, year2, month2, day2, temp = 0, res = 0;
+	struct node *temp1 = date1head;
+	if (date1head == NULL || date2head == NULL){
+		return -1;
+	}
+	//temp1->data = 0;
+
+	while (temp <= 8 && temp1->next != NULL){
+		temp++;
+		if (temp == 5){
+			year1 = temp1->data;
+		}
+		if (temp >= 5){
+			year1 = (year1)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+	}
+
+
+	//temp1 = NULL;
+	temp1 = date2head, temp = 0;
+	while (temp <= 8 && temp1->next != NULL){
+		temp++;
+		if (temp == 5){
+			year2 = temp1->data;
+		}
+		if (temp >= 5){
+			year2 = (year2)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+
+	}
+
+	temp1 = date1head;
+	temp = 0;
+	while (temp <= 4){
+		temp++;
+		if (temp == 3){
+			month1 = temp1->data;
+		}
+		if (temp >= 3 && temp<4){
+			month1 = (month1)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+
+	}
+
+
+	temp1 = date2head;
+	temp = 0;
+	while (temp <= 4){
+		temp++;
+		if (temp == 3){
+			month2 = temp1->data;
+		}
+		if (temp >= 3 && temp <4){
+			month2 = (month2)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+
+	}
+
+	temp1 = date1head;
+	temp = 0;
+	while (temp <= 2){
+		temp++;
+		if (temp == 1){
+			day1 = temp1->data;
+		};
+		if (temp >= 1 && temp < 2){
+			day1 = (day1)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+
+	}
+
+
+	temp1 = date2head;
+	temp = 0;
+	while (temp <= 2){
+		temp++;
+		if (temp == 1){
+			day2 = temp1->data;
+		}
+		if (temp >= 1 && temp<2){
+			day2 = (day2)* 10 + temp1->next->data;
+
+		}
+		temp1 = temp1->next;
+
+	}
+
+	count1 = (year1 * 365) + (month1 * 31) + (day1);
+	count2 = (year2 * 365) + (month2 * 31) + (day2);
+
+	res = count2 - count1;
+	return res - 1;
+
+
+
+
+	//return -1;
 }
